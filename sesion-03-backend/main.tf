0f2d05737635ca7cd483334599ead81a9a2b3be4
Henlo Matthee Swart <henlomatt@gmail.com>
2026-06-17 14:10:25 +0200

    refactor: modularizar backend s3 y dynamodb

--- sesion-03-backend/main.tf
+++ sesion-03-backend/main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "mi-proyecto-terraform-2026-henlomatt" # El nombre que creaste
+    bucket         = "mi-proyecto-terraform-2026-henlomatt"
     key            = "terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "terraform-lock" # La tabla que creaste
+    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
@@ -12,19 +12,8 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-# (Mantén tus recursos de s3 y dynamodb aquí abajo)
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "mi-proyecto-terraform-2026-henlomatt" # Debe ser un nombre único globalmente
-}
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+module "backend" {
+  source      = "./modules/backend"
+  bucket_name = "mi-proyecto-terraform-2026-henlomatt"
+  table_name  = "terraform-lock"
 }
---
+++ sesion-03-backend/modules/backend/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  description = "Nombre del bucket S3"
+  type        = string
+}
+
+variable "table_name" {
+  description = "Nombre de la tabla DynamoDB"
+  type        = string
+}
