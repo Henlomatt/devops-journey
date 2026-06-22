0f4d2f1a474ebe40510683359516574d7a710a75
Henlo Matthee Swart <henlomatt@gmail.com>
2026-06-22 14:24:52 +0200

    feat: implementar outputs en modulo backend

--- sesion-03-backend/modules/backend/outputs.tf
+++ sesion-03-backend/modules/backend/outputs.tf
@@ -1,3 +1,9 @@
 output "s3_bucket_arn" {
-  value = aws_s3_bucket.terraform_state.arn
+  description = "ARN del bucket S3 de estado"
+  value       = aws_s3_bucket.terraform_state.arn
+}
+
+output "dynamodb_table_name" {
+  description = "Nombre de la tabla de bloqueo DynamoDB"
+  value       = aws_dynamodb_table.terraform_lock.name
 }
